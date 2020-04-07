8b81f82af8781b0aa14bbc87e955f129a892fe9f
Matias  Cornejo lazcano <matiascornejolazcano@MacBook-Air-de-Matias.local>
2020-04-07 04:26:58 -0400

    second commit

---
+++ lab_3MatiasCornejo/lab_3MatiasCornejo/Cliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+namespace lab_3MatiasCornejo
+{
+    public class client : Person
+    {
+
+        public string Compras;
+
+        public int Cant;
+
+
+
+        public client(string Name, string Rut, string Compras, int Cant) : base(Name, Rut)
+
+        {
+            this.Cant = Cant;
+            this.Compras = Compras;
+
+        }
+        public override void Greet()
+        {
+            base.Greet();
+
+            Console.WriteLine("jjxjcjc" + Cant);
+
+        }
+
+        public string Getcompras() { return Compras; }
+        public void Setcompras(string Compras) { this.Compras = Compras; }
+
+        public int Getcant() { return Cant; }
+        public void Setcant(int Cant) { this.Cant = Cant; }
+
+
+
+    }
+}
+
---
+++ lab_3MatiasCornejo/lab_3MatiasCornejo/Product.cs
@@ -0,0 +1,42 @@
+﻿using System;
+namespace lab_3MatiasCornejo
+{
+    public class Product
+    {
+        private string NameProduct;
+        private string Brand;//marca
+        private int Stock;
+        private int Price; //precio
+
+        public Product(string NameProduct, string Brand, int Stock, int Price)
+
+
+        {
+            this.NameProduct = NameProduct;
+            this.Brand = Brand;
+            this.Stock = Stock;
+            this.Price = Price;
+
+        }
+        public virtual void Greet()
+        {
+
+            Console.WriteLine("se han añadido " + Stock + "  " + NameProduct);
+        }
+        public int Getprice() { return Price; }
+        public void Setprice(int Price) { this.Price = Price; }
+
+        public int Getstock() { return Stock; }
+        public void Setstock(int Stock) { this.Stock = Stock; }
+
+        public string Getnameproduct() { return NameProduct; }
+        public void Setnameproduct(string NameProduct) { this.NameProduct = NameProduct; }
+
+        public string Getbrand() { return Brand; }
+        public void Setpbrand(string Brand) { this.Brand = Brand; }
+
+
+
+    }
+}
+
